e123fc41377d0ab7f58647a167aab21b47b292ed
William Novak <william.novak@sft.inf.br>
2023-06-16 16:53:32 -0300

    convert image to pcx

--- datamaxPrint.py
+++ datamaxPrint.py
@@ -1,6 +1,15 @@
 import win32print
 from PIL import Image
 
+printer_name = win32print.GetDefaultPrinter()
+
+image = Image.open('LOGO_EMP.jpg','r')
+pcxImage = image.convert('1')
+pcxImage.save('LOGO_EMP.pcx','PCX')
+
+with open('LOGO_EMP.pcx', 'rb') as file:
+    pcx_data = file.read()
+
 dpl_code  = "\x02O0220\n"
 dpl_code += "\x02M3000\n"
 dpl_code += "\x02c0000\n"
@@ -14,7 +23,7 @@ dpl_code += "PI\n"
 dpl_code += "R0003\n"
 dpl_code += "z\n"
 dpl_code += "W\n"
-# dpl_code += "^012Y1100000170192LOGO_EMP\n"
+dpl_code += "^012Y1100000170192LOGO_EMP\n"
 dpl_code += "221100200730177169.001\n"
 dpl_code += "221100200640167V: 1\n"
 dpl_code += "Q0001\n"
@@ -23,11 +32,10 @@ dpl_code += "E"
 dpl_code_bytes = str(dpl_code).encode(encoding='cp437')
 # print(dpl_code)
 
-# printer_name = win32print.GetDefaultPrinter()
-# printer = win32print.OpenPrinter(printer_name)
-# job = win32print.StartDocPrinter(printer, 1, ("Label", None, "RAW"))
-# win32print.StartPagePrinter(printer)
-# win32print.WritePrinter(printer, dpl_code_bytes)
-# win32print.EndPagePrinter(printer)
-# win32print.EndDocPrinter(printer)
-# win32print.ClosePrinter(printer)
+printer = win32print.OpenPrinter(printer_name)
+job = win32print.StartDocPrinter(printer, 1, ("Label", None, "RAW"))
+win32print.StartPagePrinter(printer)
+win32print.WritePrinter(printer, dpl_code_bytes)
+win32print.EndPagePrinter(printer)
+win32print.EndDocPrinter(printer)
+win32print.ClosePrinter(printer)

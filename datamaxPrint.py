693106e0ed7450159267d8c77a778ab3372374c4
William Novak <william.novak@sft.inf.br>
2023-06-19 15:59:43 -0300

    testes imagem

--- datamaxPrint.py
+++ datamaxPrint.py
@@ -10,29 +10,48 @@ pcxImage.save('LOGO_EMP.pcx','PCX')
 with open('LOGO_EMP.pcx', 'rb') as file:
     pcx_data = file.read()
 
-dpl_code  = "\x02O0220\n"
-dpl_code += "\x02M3000\n"
-dpl_code += "\x02c0000\n"
-dpl_code += "\x02f322\n"
-dpl_code += "\x02e\n"
-dpl_code += "\x02LC0000\n"
-dpl_code += "H08\n"
-dpl_code += "D11\n"
-dpl_code += "SG\n"
-dpl_code += "PI\n"
-dpl_code += "R0003\n"
-dpl_code += "z\n"
-dpl_code += "W\n"
+print(pcx_data.decode('latin-1'))
+
+# dpl_code  = "\x02IBPLOGO_EMP\n" + pcx_data.decode('latin-1') + "\n"
+# dpl_code += "\x02O0220\n"
+# dpl_code += "\x02M3000\n"
+# dpl_code += "\x02c0000\n"
+# dpl_code += "\x02f322\n"
+# dpl_code += "\x02e\n"
+# dpl_code += "\x02LC0000\n"
+# dpl_code += "H08\n"
+# dpl_code += "D11\n"
+# dpl_code += "SG\n"
+# dpl_code += "PI\n"
+# dpl_code += "R0003\n"
+# dpl_code += "z\n"
+# dpl_code += "W\n"
 # dpl_code += "^012Y1100000170192LOGO_EMP\n"
+# dpl_code += "1Y1100001300140LOGO_EMP\n"
 # dpl_code += "1W1D44000001000102\n" # qrCode
 # dpl_code += "1D000000015010001234567890\n" # barCode
-dpl_code += "221100200730177169.001\n"
-dpl_code += "221100200640167V: 1\n"
+# dpl_code += "221100200730177169.001\n"
+# dpl_code += "221100200640167V: 1\n"
+# dpl_code += "Q0001\n"
+# dpl_code += "E"
+
+dpl_code  = "\x02O0220\n"
+dpl_code += "\x02f220\n"
+dpl_code += "\x02e\n"
+dpl_code += "\x02xBGPB0\n"
+dpl_code += "\x02IBPPB0\n"
+dpl_code += pcx_data.decode('latin-1') + "\x02L\n"
+dpl_code += "H10\n"
+dpl_code += "PC\n"
+dpl_code += "A2\n"
+dpl_code += "D11\n"
+dpl_code += "1Y1100001300140PB0\n"
+dpl_code += "^01\n"
 dpl_code += "Q0001\n"
 dpl_code += "E"
 
-dpl_code_bytes = str(dpl_code).encode(encoding='cp437')
-# print(dpl_code)
+dpl_code_bytes = str(dpl_code).encode(encoding='latin-1')
+print(dpl_code)
 
 printer = win32print.OpenPrinter(printer_name)
 job = win32print.StartDocPrinter(printer, 1, ("Label", None, "RAW"))
